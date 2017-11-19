36457f9d0c5bb6a309a2e3d2425496e9d5655724
Chinnawat K <chinnawatsut@gmail.com>
2017-11-19 19:11:30 +0700

    pass case 2

--- golang/fizzBuzz/fizzBuzz.go
+++ golang/fizzBuzz/fizzBuzz.go
@@ -1,5 +1,8 @@
 package fizzBuzz
 
 func fizzBuzz(a int) string {
+	if a == 2 {
+		return "2"
+	}
 	return "1"
 }
--- golang/fizzBuzz/fizzBuzz_test.go
+++ golang/fizzBuzz/fizzBuzz_test.go
@@ -2,8 +2,14 @@ package fizzBuzz
 
 import "testing"
 
-func TestSayOne(t *testing.T) {
+func TestSay1(t *testing.T) {
 	if fizzBuzz(1) != "1" {
 		t.Error("Expecting ", "1", "but got", fizzBuzz(1))
+	}
+}
+
+func TestSay2(t *testing.T) {
+	if fizzBuzz(2) != "2" {
+		t.Error("Expecting ", "2", "but got", fizzBuzz(2))
 	}
 }
