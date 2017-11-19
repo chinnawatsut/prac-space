d1509b360e4aab7636f446a280744c26a243003e
Chinnawat K <chinnawatsut@gmail.com>
2017-11-19 19:21:39 +0700

    test 4 pass

--- golang/fizzBuzz/fizzBuzz_test.go
+++ golang/fizzBuzz/fizzBuzz_test.go
@@ -14,6 +14,10 @@ func TestSayFizz(t *testing.T) {
 	compare(fizzBuzz(3), "fizz", t)
 }
 
+func TestSay4(t *testing.T) {
+	compare(fizzBuzz(4), "4", t)
+}
+
 func compare(result, expecting string, t *testing.T) {
 	if result != expecting {
 		t.Error("Expecting ", expecting, "but got", result)
